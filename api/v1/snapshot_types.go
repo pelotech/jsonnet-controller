33c96c8a4aeaf0f9a8393cdeb23c0de91f9ed054
Avi Zimmerman <avi.zimmerman@gmail.com>
2021-06-08 11:19:27 +0300

    fix derived license headers

--- api/v1/snapshot_types.go
+++ api/v1/snapshot_types.go
@@ -1,6 +1,18 @@
 /*
 Copyright 2020 The Flux authors
-		  2021 Avi Zimmerman.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Copyright 2021 Avi Zimmerman.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,6 +25,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+ - Adaption from fluxcd/kustomize-controller for kubecfg
 */
 
 package v1
